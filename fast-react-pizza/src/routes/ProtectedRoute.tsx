1bcfbb06bd5d5b742fb0746b203932fdaa710d49
m1rade <forx98@gmail.com>
2024-04-25 02:37:24 +0300

    eslint fix

--- fast-react-pizza/src/routes/ProtectedRoute.tsx
+++ fast-react-pizza/src/routes/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import type React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../app/hooks';
 
 type Props = {
